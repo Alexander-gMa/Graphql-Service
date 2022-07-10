c44b63034f51fb377b4dfc27da8a8f8624a538d3
Alexander-gMa <82538974+Alexander-gMa@users.noreply.github.com>
2022-07-10 09:17:25 +0300

    feat: add ResolveField for album

--- src/modules/albums/album.module.ts
+++ src/modules/albums/album.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ArtistService } from '../artists/services/artist.service';
+import { BandService } from '../bands/services/band.service';
+import { GenreService } from '../genres/services/genre.service';
+import { TrackService } from '../tracks/services/track.service';
 import { AlbumResolver } from './resolvers/album.resolver';
 import { AlbumService } from './services/album.service';
 
 @Module({
-    providers: [AlbumService, AlbumResolver],
+    providers: [AlbumService, AlbumResolver, ArtistService, BandService, TrackService, GenreService],
 })
 export class AlbumModule { }
--- src/modules/albums/resolvers/album.resolver.ts
+++ src/modules/albums/resolvers/album.resolver.ts
@@ -1,10 +1,20 @@
 import { Args, Mutation, Resolver, Query, Context, ResolveField, Parent } from '@nestjs/graphql';
 import { AlbumService } from '../services/album.service';
 import { CreateAlbumInput, UpdateAlbumInput } from 'src/graphql';
+import { ArtistService } from 'src/modules/artists/services/artist.service';
+import { BandService } from 'src/modules/bands/services/band.service';
+import { TrackService } from 'src/modules/tracks/services/track.service';
+import { GenreService } from 'src/modules/genres/services/genre.service';
 
 @Resolver('Album')
 export class AlbumResolver {
-    constructor(private albumService: AlbumService) { }
+    constructor(
+        private albumService: AlbumService,
+        private artistService: ArtistService,
+        private bandService: BandService,
+        private trackService: TrackService,
+        private genreService: GenreService,
+        ) { }
 
     @Query('albums')
     async getAllArtist(
@@ -43,6 +53,40 @@ export class AlbumResolver {
     @ResolveField()
     async id(@Parent() album): Promise<string> {
         return album._id;
+    }
+
+    @Resolver()
+    @ResolveField()
+    async artists(
+        @Parent() album,
+    ) {
+        return await Promise.all(album.artistsIds.map(id => this.artistService.getArtistByID(id)));
+    }
+
+    @Resolver()
+    @ResolveField()
+    async bands(
+        @Parent() album,
+    ) {
+        return await Promise.all(album.bandsIds.map((id) => this.bandService.getBandByID(id)));
+    }
+
+    @Resolver()
+    @ResolveField()
+    async tracks(
+        @Parent() album,
+    ) {
+        return await Promise.all(album.trackIds.map((id) => this.trackService.getTrackByID(id)));
+    }
+
+    @Resolver()
+    @ResolveField()
+    async genres(
+        @Parent() album,
+    ) {
+        const { genresIds } = album;
+        const filter = genresIds.filter((el)=> el!== 123)
+        return await Promise.all(filter.map((id) => this.genreService.getGenreByID(id)));
     }
 
 }
